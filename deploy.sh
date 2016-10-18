b99cdfd97139a6703bee8d69d4b3835096d41c2c
Peter Wiggers <ppa.wiggers@gmail.com>
2016-10-18 22:11:02 +0200

    disable docker cache, use env vars in deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ do
   envsubst < $f > "${TARGET_DIR}/$(basename $f)"
 done
 
-gcloud container clusters get-credentials cluster-1 --zone europe-west1-d --project testing-145512
+gcloud container clusters get-credentials ${GC_CLUSTER_NAME} --zone ${GC_COMPUTE_ZONE} --project ${GC_PROJECT_NAME}
 
 # quick and dirty recreating configmaps
 kubectl delete --namespace=testing-kubernetes configmap nginx-config
