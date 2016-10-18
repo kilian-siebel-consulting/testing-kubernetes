8528c77b5b1434941a622dea051f013c1b1e2f84
Peter Wiggers <ppa.wiggers@gmail.com>
2016-10-18 18:19:13 +0200

    new feature for my app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World, Hello Travis!');
+  res.send('Hello World, Hello Travis!!! CI');
 });
 
 app.listen(3000, '0.0.0.0', function () {
