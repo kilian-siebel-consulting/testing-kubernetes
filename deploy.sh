4b40bb19df16f651dca9222edc9030b4b3664ef1
Peter Wiggers <ppa.wiggers@gmail.com>
2016-10-18 22:01:12 +0200

    cleanup

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-TAG=${1}
 K8S_DIR=./conf/k8s
 TARGET_DIR=${K8S_DIR}/.generated
 mkdir -p ${TARGET_DIR}
-export BUILD_NUMBER=${TAG}
+export BUILD_NUMBER=${1}
 for f in ./conf/k8s/*.yaml
 do
   envsubst < $f > "${TARGET_DIR}/$(basename $f)"
 done
 
-#gcloud container clusters get-credentials cluster-1 --zone europe-west1-d --project testing-145512
+gcloud container clusters get-credentials cluster-1 --zone europe-west1-d --project testing-145512
 
 # quick and dirty recreating configmaps
 kubectl delete --namespace=testing-kubernetes configmap nginx-config
