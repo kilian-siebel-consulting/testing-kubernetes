1544af446b53d8b503f7da38167be991216f894f
Peter Wiggers <ppa.wiggers@gmail.com>
2016-10-19 12:02:37 +0200

    show hostname for testing scaling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+var os = require("os");
 
 app.get('/', function (req, res) {
-  res.send('Hello World, Hello Travis!!! CI');
+  res.send('Hello World! My hostname is ' + os.hostname());
 });
 
 app.listen(3000, '0.0.0.0', function () {
